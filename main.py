b0d2f3bb39c0a147fbc18ea2c8536c9774af57f8
John Woolbright <jwoolbright23@gmail.com>
2022-03-25 13:37:22 -0500

    new feature
    feature containing a function to handle writing logfile.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import datetime
+https://launchcodetechnicaltraining.org/linux/bash-introduction/walkthrough/which/ import datetime
 import socket
 import os
 
-if __name__ == "__main__":
+def new_function(new_file_name):
+
     ts = datetime.datetime.now()
     home_dir = os.environ["HOME"]
     user = os.environ["USERNAME"]
     hostname = socket.gethostname()
     local_ip = socket.gethostbyname(hostname)
 
-    with open ("web.log", "w") as tf:
-        tf.write("{}: {}@{} ip: {} home-dir: {}".format(ts, user, hostname, local_ip, home_dir))
+    with open(new_file_name, "w") as the_file:
+        the_file.write("{}: {}@{} ip: {} home-dir: {}\n".format(ts, user, hostname, local_ip, home_dir))
+
+if __name__ == "__main__":
+    new_function("web.log")
 
